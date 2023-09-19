2738b44812d72b18ee7845ab1213efb8637d8926
Vishnushankar <vishnu@metaplayerone.app>
2023-09-19 19:10:18 +0400

    updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
+import dns from 'dns'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+dns.setDefaultResultOrder('verbatim')
+
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: 3000
+  }
 })
